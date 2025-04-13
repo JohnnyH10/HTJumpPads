7d6723d24ec126c6b82d63e4659eae840f458070
johnnyh10 <theunreadab1egamer@gmail.com>
2025-04-13 01:28:03 -0400

    main fix

--- src/main/java/me/johnnyht/race/HtRacePads.java
+++ src/main/java/me/johnnyht/race/HtRacePads.java
@@ -1,6 +1,6 @@
-package me.johnnyht.htracepads;
+package me.johnnyht.race;
 
-import me.johnnyht.htracepads.bstats.Metrics;
+import me.johnnyht.race.bstats.Metrics;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Block;
